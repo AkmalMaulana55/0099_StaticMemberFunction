8f963784b64b4e4f46cb4c6972427bc8e6a08ef1
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-05-20 16:37:02 +0700

    menambahkan class barang, private

---
+++ latiansoal.cpp
@@ -0,0 +1,15 @@
+// Buatlah program C++ dengan class Barang yang memiliki atribut: namaBarang,
+//kodeBarang
+//program memiliki constructor berparameter
+//untuk mengisi nilai namaBarang dan kodeBarang
+//serta memiliki fungsi untuk menampilkan informasi barang.
+
+#include <iostream>
+using namespace std;
+
+class Barang {
+    private:
+        string namaBarang;
+        int kodeBarang;
+    
+};
